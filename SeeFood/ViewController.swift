ec4701c2c5c4402fd135ae0366ef908b2eb9fd76
Juan Mejia <32790229+mejiajuan50@users.noreply.github.com>
2018-09-26 10:29:21 -0700

    Add Progress Alert using SVProgressHUD

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreML
 import Vision
 import ChameleonFramework
+import SVProgressHUD
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -20,7 +21,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.tintColor = UIColor.black
         photoView.image = UIImage.init(named: "hotdog")
+        
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
@@ -35,39 +38,50 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             guard let ciimage = CIImage(image: userPickedImage) else { fatalError("Could not convert to CIImage")}
             
+            SVProgressHUD.show(withStatus: "Evaluating...")
+            
             detect(image: ciimage)
         }
         
-        imagePicker.dismiss(animated: true, completion: nil)
-       
-    }
-    
-    func detect(image: CIImage) {
-    
-        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Loading CoreML Model Failed.")}
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { 
+            SVProgressHUD.dismiss()
+            self.imagePicker.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+
+    
+  
+    func detect(image: CIImage) {
+
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("Loading CoreML Model Failed.")}
+
         let request = VNCoreMLRequest(model: model) { (request, error) in
-            
+
             guard let results = request.results as? [VNClassificationObservation] else { fatalError("Model failed to process image")}
-            
+
             if let firstResult = results.first {
+                
                 if firstResult.identifier.contains("hotdog") {
-                    self.updateNavBar(withHexcode: FlatGreen().hexValue(), withId: "Hotdog!")
+                    
+                    self.updateNavBar(withHexcode: FlatGreen().hexValue())
                 }
                 else {
-                    self.updateNavBar(withHexcode: FlatRed().hexValue(), withId: "Not Hotdog!")
+                    
+                    self.updateNavBar(withHexcode: FlatRed().hexValue())
                 }
             }
         }
-        
+    
         let handler = VNImageRequestHandler(ciImage: image)
-        
+
         do {
             try handler.perform([request])
         } catch {
             print(error)
         }
-    
+
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
@@ -76,25 +90,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
-    // MARK - Nav Bar Setup Methods
+    // MARK - Update Method
     
-    func updateNavBar(withHexcode colorHexCode:String, withId id:String) {
+    func updateNavBar(withHexcode colorHexCode:String) {
         
         guard let navBar = navigationController?.navigationBar else {fatalError("Navigation Controller does not exist.")}
         
         guard let navBarColor = UIColor(hexString: colorHexCode) else {fatalError()}
         
-        navigationItem.title = id
+        navigationItem.title = colorHexCode == FlatGreen().hexValue() ? "Hotdog!" : "Not Hotdog!"
         
         navBar.barTintColor = navBarColor
-        
-        //navBar.tintColor = ContrastColorOf(navBarColor, returnFlat: true)
+    
         navBar.tintColor = UIColor.black
     
         navBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.black, NSAttributedString.Key.font : UIFont.systemFont(ofSize: 25)]
-        
-        
-        
+    
     }
     
 }
